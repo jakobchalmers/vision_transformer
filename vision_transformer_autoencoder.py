65b5882658072ce4fb0f30fd6e721240b072107c
Johan Philis <johan.e.philis@gmail.com>
2024-05-08 17:56:10 +0200

    milestone reached: transformed learnt

--- vision_transformer_autoencoder.py
+++ vision_transformer_autoencoder.py
@@ -3,43 +3,71 @@ import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch
 from torch.utils.data import DataLoader, Dataset
+import torch.optim as optim
 from tqdm import tqdm
 from data_generation import SequenceDataset, ImageDataset
 import numpy as np
 
+# %% Data Loading
+
+FILE_NAME = "data/particle_dataset_500.pth"
+TEST_FILE_NAME = "data/particle_test_dataset_100.pth"
+
+print("Loading...")
+sequence_dataset: SequenceDataset = torch.load(FILE_NAME)
+sequence_test_dataset: SequenceDataset = torch.load(TEST_FILE_NAME)
+
+
+image_dataset = ImageDataset(sequence_dataset)
+image_test_dataset = ImageDataset(sequence_test_dataset)
+
+
+train_loader = DataLoader(
+    image_dataset,
+    batch_size=32,
+    shuffle=True,
+)
+
+test_loader = DataLoader(
+    image_test_dataset,
+    batch_size=32,
+    shuffle=False,
+)
+print("Done")
+
+# %% Functions
+def train(model, train_loader, criterion, optimizer):
+    model.train()
+    total_loss = 0
+    for input in train_loader:
+        input: torch.Tensor = input.permute(0, 3, 1, 2)
+        output = model(input)
+
+        loss = criterion(input, output)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    mean_loss = total_loss / len(train_loader)
+    return mean_loss
+
+
+def test(model, data_loader, criterion):
+    model.train()
+    total_loss = 0
+    for input in data_loader:
+        input: torch.Tensor = input.permute(0, 3, 1, 2)
+        output = model(input)
+
+        loss = criterion(input, output)
+        total_loss += loss.item()
+
+    mean_loss = total_loss / len(data_loader)
+    return mean_loss
+
 # %% Modules
-# class Patcher(nn.Module):
-#     def __init__(self, patch_size: int, image_size: int, in_channels: int):
-#         super(Patcher, self).__init__()
-#         self.patch_size = patch_size
-#         self.image_size = image_size
-#         self.in_channels = in_channels
-
-#         self.patch_dim = patch_size * patch_size * in_channels
-#         self.num_patches = (image_size // patch_size) ** 2
-
-#         self.conv = nn.Conv2d(
-#             in_channels=in_channels,
-#             out_channels=self.patch_dim,
-#             kernel_size=patch_size,
-#             stride=patch_size,
-#         )
-
-#     def forward(self, x):
-#         x = self.conv(x)
-#         x = x.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size)
-#         x = x.permute(0, 2, 3, 1, 4, 5).reshape(-1, self.patch_dim, self.patch_size, self.patch_size)
-#         return x
-
-# class Patcher(nn.Module):
-#     def __init__(self):
-#         super(Patcher, self).__init__()
-
-#     def forward(self, x):
-#         x = x.unfold(2, 16, 16).unfold(3, 16, 16)
-#         x = x.permute(0, 2, 3, 1, 4, 5).reshape(-1, 1, 16, 16)
-#         return x
-
 
 class Patchify(nn.Module):
     def __init__(self, patch_size: int):
@@ -93,7 +121,6 @@ class ClassToken(nn.Module):
     def forward(self, x):
         batch_size, num_patches, patch_dim = x.shape
         class_token = self.class_token.expand(batch_size, -1, -1)
-        print(class_token.shape)
         x = torch.cat([class_token, x], dim=1)
         return x
 
@@ -127,6 +154,15 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         return x+ self.positional_encoding
 
+class ClassTokenGrabber(nn.Module):
+    def __init__(self, num_patches: int, dim_embedding: int):
+        super(ClassTokenGrabber, self).__init__()
+        self.num_patches = num_patches
+        self.dim_embedding = dim_embedding
+
+    def forward(self, x):
+        return x[:, -1, :]  # -> (batch_size, dim_embedding)
+
 
 class VisionTransformerAutoencoder(nn.Module):
     def __init__(self, dim_embedding: int):
@@ -148,52 +184,128 @@ class VisionTransformerAutoencoder(nn.Module):
             PrintLayer(identifier="positional"),
         )
 
-        # self.encoder = nn.Sequential(
-        # )
+        self.encoder = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(
+                d_model=dim_embedding,
+                nhead=2,
+                dim_feedforward=2048,
+                dropout=0.1,
+                activation="gelu",
+                norm_first=True,
+            ),
+            num_layers=4,
+        )
 
-        # self.decoder = nn.Sequential(
-        # )
+        self.class_token_grabber = ClassTokenGrabber(num_patches=num_patches, dim_embedding=dim_embedding)
+
+        activation = nn.SiLU()
+        latent_dim = dim_embedding
+        hidden_feature_dim_1 = 16
+        hidden_feature_dim_2 = 32
+        hidden_feature_dim_3 = 64
+        kernel_size = 3
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, 8 * 8 * hidden_feature_dim_3),
+            # nn.ConvTranspose1d(latent_dim, hidden_feature_dim_3, kernel_size=2),
+            activation,
+            nn.Unflatten(dim=1, unflattened_size=(hidden_feature_dim_3, 8, 8)),
+            
+
+            nn.ConvTranspose2d(
+                hidden_feature_dim_3,
+                hidden_feature_dim_2,
+                kernel_size=kernel_size,
+                stride=2,
+                padding=1,
+                output_padding=1,
+            ),
+            activation,
+
+            nn.ConvTranspose2d(
+                hidden_feature_dim_2,
+                hidden_feature_dim_1,
+                kernel_size=kernel_size,
+                stride=2,
+                padding=1,
+                output_padding=1,
+            ),
+            activation,
+
+            nn.ConvTranspose2d(
+                hidden_feature_dim_1,
+                1,
+                kernel_size=kernel_size,
+                stride=2,
+                padding=1,
+                output_padding=1,
+            ),
+            # activation,
+
+            # nn.Conv2d(
+            #    in_channels=4,
+            #    out_channels=1,
+            #    kernel_size=3,
+            #    stride=1,
+            #    padding=1,
+            # ),
+            # nn.Sigmoid(),
+            
+            PrintLayer(identifier="output"),
+        )
 
     def forward(self, x):
         x = self.embedding(x)
-        # x = self.encoder(x)
-        # x = self.decoder(x)
+        x = self.encoder(x)
+        x = self.class_token_grabber(x)
+        x = self.decoder(x)
         return x
-
-
-# %% Data Loading
-
-FILE_NAME = "data/particle_dataset_500.pth"
-TEST_FILE_NAME = "data/particle_test_dataset_100.pth"
-
-print("Loading...")
-sequence_dataset: SequenceDataset = torch.load(FILE_NAME)
-sequence_test_dataset: SequenceDataset = torch.load(TEST_FILE_NAME)
-
-
-image_dataset = ImageDataset(sequence_dataset)
-image_test_dataset = ImageDataset(sequence_test_dataset)
-
-
-train_loader = DataLoader(
-    image_dataset,
-    batch_size=32,
-    shuffle=True,
-)
-
-test_loader = DataLoader(
-    image_test_dataset,
-    batch_size=32,
-    shuffle=False,
-)
 
 # %% test on data
 
-model = VisionTransformerAutoencoder(dim_embedding=3)
+model = VisionTransformerAutoencoder(dim_embedding=4)
 
 for batch in train_loader:
     input = batch.permute(0, 3, 1, 2)
     output = model(input)
     print(output.shape)
+    plt.imshow(output[0, 0].detach().numpy())
+    break
+
+# %% Training
+
+model = VisionTransformerAutoencoder(dim_embedding=4)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+print(model)
+
+initial_train_loss = test(model, train_loader, criterion)
+print(f"{initial_train_loss=}")
+initial_test_loss = test(model, test_loader, criterion)
+print(f"{initial_test_loss=}")
+
+num_epochs = 20
+# %%
+for i, epoch in tqdm(enumerate(range(num_epochs))):
+    train_loss = train(model, train_loader, criterion, optimizer)
+    test_loss = test(model, test_loader, criterion)
+    
+    print(f"Epoch {i+1+20}: Train loss {train_loss}, Test Loss {test_loss}")
+
+# %% plot
+for data in test_loader:
+    img = data[torch.randint(low=0, high=31, size=(1,)).item(), :, :, :]
+    print(img.shape)
+    output_img = model.forward(img.permute(2, 0, 1).unsqueeze(0))
+    output_img = output_img.detach().squeeze(0).permute(1, 2, 0)
+    figure = plt.figure()
+    subplot1 = figure.add_subplot(1, 2, 1)
+    subplot1.imshow(img)
+    subplot1.set_title('Original Image')
+    
+    subplot2 = figure.add_subplot(1, 2, 2)
+    subplot2.imshow(output_img)
+    subplot2.set_title('Output Image')
+    
+    plt.show()
 
     break
